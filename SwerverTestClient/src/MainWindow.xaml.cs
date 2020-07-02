aacdaf4834f2b5136f5ba82f7a203c97597d63df
TheSwerik <derswerik@gmail.com>
2020-07-02 13:14:24 +0200

    fix testproject

---
+++ SwerverTestClient/src/MainWindow.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace SwerverTestClient
+{
+    /// <summary>
+    ///     Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() { InitializeComponent(); }
+    }
+}
--- SwerverTestClient/src/UdpImpl.cs
+++ SwerverTestClient/src/UdpImpl.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using Swerver.Util;
-
-namespace SwerverTestClient
+﻿namespace SwerverTestClient
 {
     public class UdpImpl : Udp
     {
-        public UdpImpl(string ip, int port) : base(ip, port) { }
-
-        protected override void ExecuteOnMainThread(Action action) { }
     }
 }
