b02673703b8dca8307aff9c9bc10ce5337a20549
TheSwerik <derswerik@gmail.com>
2020-07-03 13:15:42 +0200

    fix Test

--- SwerverTestClient/src/MainWindow.xaml.cs
+++ SwerverTestClient/src/MainWindow.xaml.cs
@@ -21,7 +21,13 @@ namespace SwerverTestClient
 
         private void Window_OnClosing(object sender, CancelEventArgs e) { Client.Instance.Disconnect(); }
         private void Button_Disconnect_OnClick(object sender, RoutedEventArgs e) { Client.Instance.Disconnect(); }
-        private void Button_Connect_OnClick(object sender, RoutedEventArgs e) { Client.Instance.ConnectToServer(); }
+
+        private void Button_Connect_OnClick(object sender, RoutedEventArgs e)
+        {
+            Client.Instance.ConnectToServer();
+            Client.PacketHandlers.Add((int) PacketEnum.Lol, SendAndHandel.ReceiveLol);
+        }
+
         private void Button_lol_OnClick(object sender, RoutedEventArgs e) { SendAndHandel.SendLol(); }
     }
 }
